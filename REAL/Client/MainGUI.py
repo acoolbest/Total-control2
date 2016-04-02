91df9dd85badb4aa5e5a819447a14b1c0e6cc8e8
Nelicia <gofk801@gmail.com>
2016-04-03 07:32:23 +0900

    setting.py 구현 -설정파일 불러오고 저장하는 부분 구현

--- REAL/Client/MainGUI.py
+++ REAL/Client/MainGUI.py
@@ -5,6 +5,7 @@ import SettingGUI
 class MainGUI():
     def __init__(self):
         self.app=Tk()
+        self.app.title('Total control v1.0')
         connectButton=Button(self.app,text='connect')
         disconnectButton=Button(self.app,text='disconnect')
         settingButton=Button(self.app,text='setting')
@@ -12,8 +13,13 @@ class MainGUI():
         disconnectButton.grid(row=0,column=1)
         settingButton.grid(row=0,column=2)
 
+        settingButton.bind('<Button-1>', self.settingButtonCallback)
         self.app.mainloop()
         pass
 
     def __del__(self):
+        pass
+
+    def settingButtonCallback(self,event):
+        SettingGUI.SettingGUI(self.app)
         pass
--- REAL/Client/SettingGUI.py
+++ REAL/Client/SettingGUI.py
@@ -1,12 +1,58 @@
 from Tkinter import *
 
-class SettingGUI():
+class SettingGUI(Toplevel):
     def __init__(self,parent):
-        self.window=Toplevel(parent,text="setting")
+        Toplevel.__init__(self)
+        self.title('Setting')
+        self.startupCheckFlag=IntVar()
+        self.ipEntryString=StringVar()
+        self.portEntryString=StringVar()
+
+        self.ipEntry=Entry(self,textvariable=self.ipEntryString)
+        self.portEntry=Entry(self,textvariable=self.portEntryString)
+
+        self.okButton=Button(self,text='OK')
+        self.cancelButton=Button(self,text='cancel')
+
+        self.startupCheck=Checkbutton(self,text='startup',variable=self.startupCheckFlag)
+
+        self.ipEntry.grid(row=0,column=0)
+        self.portEntry.grid(row=1,column=0)
+        self.okButton.grid(row=0,column=1)
+        self.cancelButton.grid(row=1,column=1)
+        self.startupCheck.grid(row=2,column=0)
+
+        self.okButton.bind('<Button-1>',self.okButtonCallback)
+        self.loadSetting()
         pass
 
     def __del__(self):
         pass
 
+    def okButtonCallback(self,evnet):
+        print 'okButton push'
+        file=open('setting.set','wt')
+        file.write(self.ipEntry.get()+'\n')
+        file.write(self.portEntry.get()+'\n')
+        file.write(str(self.startupCheckFlag.get()))
+
+        print "ip: ",self.ipEntry.get()
+        print "port: ",self.portEntry.get()
+        print self.startupCheckFlag.get()
+        file.close()
+
+    def loadSetting(self):
+
+
+        file=open('setting.set','rt')
+        import os
+        if os.path.getsize('setting.set'):
+
+            self.ipEntryString.set(file.readline().strip())
+            self.portEntryString.set(file.readline().strip())
+            self.startupCheckFlag.set(int(file.readline()))
+        file.close()
+
+        pass
 
 
